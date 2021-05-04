313b9b8d67162e3e589c735a41fe23274022e0d0
rcortez <renecortez@banpro.com.ni>
2021-05-04 00:51:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Router, Switch, Route } from "react-router-dom";
+import Dashboard from './components/Dashboard/Dashboard';
+import NotFound from './components/NotFound/NotFound';
+import Login from './components/Login/Login'
+import SignUp from './components/SignUp/SignUp'
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute"
+import { clearMessage } from "./actions/message";
+import { history } from "./helpers/history";
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen(() => {
+      dispatch(clearMessage()); // clear message when changing location
+    });
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <PrivateRoute exact path={["/", "/stats"]} component={Dashboard} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
